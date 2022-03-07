4d4280018376346265403e04c7a18060b4ea626a
KncskBenedek <95653038+KncskBenedek@users.noreply.github.com>
2022-03-07 15:27:14 +0100

    javítás

--- src/oraimunka0307/OraiMunka0307.java
+++ src/oraimunka0307/OraiMunka0307.java
@@ -5,7 +5,7 @@ public class OraiMunka0307 {
 
     public static void main(String[] args) {
 
-        String nev1 = "koncsik benede"; 
+        String nev1 = "koncsik benedek"; 
         String nev2 = "dervalics attila"; 
         String nev3 = "Ódor norbert";
 
