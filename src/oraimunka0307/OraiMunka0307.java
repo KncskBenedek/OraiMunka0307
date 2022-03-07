3a4959dfa70d1a7ab8a54c42020dce7a8bf14ac1
KncskBenedek <koncsikbenedek@gmail.com>
2022-03-07 15:23:27 +0100

    összes nev inicializálása

--- src/oraimunka0307/OraiMunka0307.java
+++ src/oraimunka0307/OraiMunka0307.java
@@ -4,7 +4,10 @@ package oraimunka0307;
 public class OraiMunka0307 {
 
     public static void main(String[] args) {
-        String nev1, nev2, nev3;
+        String nev1 = "koncsik benede"; 
+        String nev2 = "dervalics attila"; 
+        String nev3 = "Ódor norbert";
+
     }
     
 }
